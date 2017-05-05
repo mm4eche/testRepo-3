ed401e5a4e8f92726b651db5a27826d080e500fa
mm4eche <userktkn@some.mail>
2017-05-05 15:50:44 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1492700988677
+//first_change1493992996891
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
